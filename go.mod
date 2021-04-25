3fbccae57579439596445b5a5bcb7ab8e9aa3bc5
Dewmal <dewmal@egreen.io>
2021-04-25 07:45:15 +0530

    Add project details

---
+++ go.mod
@@ -0,0 +1,10 @@
+module binancereader.com/m
+
+go 1.16
+
+require (
+	github.com/go-redis/redis/v8 v8.8.2 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/kisielk/og-rek v1.1.0 // indirect
+	go.opentelemetry.io/otel v0.20.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"github.com/go-redis/redis/v8"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/url"
+	"os"
+	"os/signal"
+	"sync"
+)
+
+var addr = flag.String("bi", "stream.binance.com:9443", "binance webservice address")
+var redisUrl = flag.String("redis", "127.0.0.1:6379", "Redis server address")
+var redisPassword = flag.String("redis-pw", "", "Redis server password")
+var redisDb = flag.Int("redis-db", 0, "Redis server password")
+var channelNameFlag = flag.String("redis-channel", "BinanceAPI", "Redis server password")
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+
+	ctx := context.Background()
+	channelName := *channelNameFlag
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     *redisUrl,
+		Password: *redisPassword, // no password set
+		DB:       *redisDb,       // use default DB
+	})
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{Scheme: "wss", Host: *addr, Path: "/ws"}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			rdb.Publish(ctx, channelName, message)
+		}
+		wg.Done()
+	}()
+
+	b := []byte(
+		`{
+  "method": "SUBSCRIBE",
+  "params":["!ticker@arr"],
+  "id":1
+}`)
+	var f interface{}
+	err = json.Unmarshal(b, &f)
+	if err != nil {
+		log.Println(err)
+	}
+	err = c.WriteJSON(f)
+	if err != nil {
+		log.Println(err)
+	}
+	wg.Wait()
+}
